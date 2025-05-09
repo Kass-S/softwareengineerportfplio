4da7ee88c8b1953039c40fe4336b2ba3f484fe21
Kass-S <kassiespacek101@gmail.com>
2025-05-09 12:56:19 -0700

    projec info

--- src/components/PageComponent.tsx
+++ src/components/PageComponent.tsx
@@ -1,10 +1,20 @@
-import React from 'react'
+'use client'
+
+import React, { useState } from 'react'
 import DeveloperCard from './DeveloperCard'
+import ProjectComponent from './ProjectComponent';
 
 const PageComponent = () => {
+    const [projectsHidden, setProjectsHidden] = useState<boolean>(false);
+
+    const handleSwitch = () => {
+        setProjectsHidden(!projectsHidden);
+      }
   return (
     <div>
-        <DeveloperCard />
+        {
+            projectsHidden ? <DeveloperCard /> : <ProjectComponent />
+        }
     </div>
   )
 }
--- src/components/ProjectComponent.tsx
+++ src/components/ProjectComponent.tsx
@@ -1,14 +1,29 @@
-'use client'
-
 import React from 'react'
 
 const ProjectComponent = () => {
   return (
-    <div>
+    <div className='min-w-[28rem] max-w-[40rem] m-30  p-4'>
+
+        <h1 className='text-5xl mb-10'>Recent Projects</h1>
+
         <div>
-            <p>March 2025 - Present</p>
-            <p>Itinera</p>
-            {/* <a href="https://github.com/kcadavos/itinera-io"></a> */}
+            <div className='my-5'>
+                <p>March 2025 - Present</p>
+                <p>Itinera</p>
+                <a href="https://github.com/kcadavos/itinera-io" target='blank' className='underline hover:text-blue-500'>https://github.com/kcadavos/itinera-io</a>
+            </div>
+
+            <div className='my-5'>
+                <p>March 2025</p>
+                <p>Pokemon Search API</p>
+                <a href="https://github.com/Kass-S/pokemon-api" target='blank' className='underline hover:text-blue-500'>https://github.com/Kass-S/pokemon-api</a>
+            </div>
+
+            <div className='my-5'>
+                <p>January 2025</p>
+                <p>Rock Paper Scissors Lizard Spock</p>
+                <a href="https://github.com/Kass-S/RPSLSWeb" target='blank' className='underline hover:text-blue-500'>https://github.com/Kass-S/RPSLSWeb</a>
+            </div>
         </div>
     </div>
   )
